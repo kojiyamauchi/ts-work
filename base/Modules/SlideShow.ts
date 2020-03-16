81ce551c24d7f948824799954e31c8435d89492e
kojiyamauchi <all.the.small.things@me.com>
2020-03-16 13:07:14 +0900

    refactor.

--- base/Modules/SlideShow.ts
+++ base/Modules/SlideShow.ts
@@ -9,19 +9,12 @@ import Swiper from 'swiper/dist/js/swiper.min.js'
 export default class SlideShow {
   private playingSwiper: boolean
   private slideContainer: HTMLElement | null
-  private indexDictionary: { [key: number]: string }
+  private indexArr: string[]
 
   public constructor(slideContainer: HTMLElement) {
     this.playingSwiper = true
     this.slideContainer = slideContainer
-    this.indexDictionary = {
-      0: 'zero',
-      1: 'first',
-      2: 'second',
-      3: 'third',
-      4: 'fourth',
-      5: 'fifth'
-    }
+    this.indexArr = ['zero', 'first', 'second', 'third', 'fourth', 'fifth']
   }
 
   public core(): void {
@@ -37,14 +30,14 @@ export default class SlideShow {
   }
 
   public optionAddIndex(): void {
-    this.slideContainer!.classList.add(`is-slide-index-${this.indexDictionary[0]}`)
+    this.slideContainer!.classList.add(`is-slide-index-${this.indexArr[0]}`)
     setTimeout((): void => {
       const mySwiper = this.slideContainer!.swiper
       mySwiper.on('slideChange', (): void => {
         Array.from(document.querySelectorAll('.swiper-pagination-bullet'), (_info, index): void => {
-          this.slideContainer!.classList.remove(`is-slide-index-${this.indexDictionary[index]}`)
+          this.slideContainer!.classList.remove(`is-slide-index-${this.indexArr[index]}`)
         })
-        this.slideContainer!.classList.add(`is-slide-index-${this.indexDictionary[mySwiper.realIndex]}`)
+        this.slideContainer!.classList.add(`is-slide-index-${this.indexArr[mySwiper.realIndex]}`)
       })
     }, 2500)
   }
